b4b202f96b3465bc3032659b6f0f3d996287f3ec
Njegos Petrovic <pnjegosh@gmail.com>
2026-01-12 14:49:52 +0100

    Add React router

--- packages/drone-rental-app/src/App.tsx
+++ packages/drone-rental-app/src/App.tsx
@@ -1,24 +1,22 @@
-import {useStore} from './store/useStore';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import {BrowsePage} from './pages/BrowsePage';
 import {VerifyPage} from './pages/VerifyPage';
 import {ResultPage} from './pages/ResultPage';
 import {CheckoutPage} from './pages/CheckoutPage';
 
 function App() {
-    const {currentStep} = useStore();
-
-    switch (currentStep) {
-        case 'browse':
-            return <BrowsePage />;
-        case 'verify':
-            return <VerifyPage />;
-        case 'result':
-            return <ResultPage />;
-        case 'checkout':
-            return <CheckoutPage />;
-        default:
-            return <BrowsePage />;
-    }
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<BrowsePage />} />
+                <Route path="/browse" element={<BrowsePage />} />
+                <Route path="/verify" element={<VerifyPage />} />
+                <Route path="/result" element={<ResultPage />} />
+                <Route path="/checkout" element={<CheckoutPage />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
