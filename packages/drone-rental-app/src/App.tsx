ad7de57ede41f781e3474d7e0e92c645aefc378e
Njegos Petrovic <pnjegosh@gmail.com>
2026-01-13 13:32:31 +0100

    Update routes

--- packages/drone-rental-app/src/App.tsx
+++ packages/drone-rental-app/src/App.tsx
@@ -1,25 +1,42 @@
-import {createBrowserRouter, RouterProvider} from 'react-router';
-import {BrowsePage} from './routes/Browse';
-import {CheckoutPage} from './routes/CheckoutPage';
-import {ResultPage} from './routes/ResultPage';
-import {VerifyPage} from './routes/VerifyPage';
+import {createBrowserRouter, RouterProvider, Navigate} from 'react-router';
+import {Layout} from './components/Layout';
+import {ErrorPage} from './pages/ErrorPage';
+import {Browse} from './routes/Browse';
+import {Verify} from './routes/Verify';
+import {Result} from './routes/Result';
+import {Checkout} from './routes/Checkout';
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <BrowsePage />,
-    },
-    {
-        path: '/verify',
-        element: <VerifyPage />,
-    },
-    {
-        path: '/result',
-        element: <ResultPage />,
-    },
-    {
-        path: '/checkout',
-        element: <CheckoutPage />,
+        element: <Layout />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                index: true,
+                element: <Navigate to='/browse' replace />,
+            },
+            {
+                path: 'browse',
+                element: <Browse />,
+            },
+            {
+                path: 'verify',
+                element: <Verify />,
+            },
+            {
+                path: 'result',
+                element: <Result />,
+            },
+            {
+                path: 'checkout',
+                element: <Checkout />,
+            },
+            {
+                path: '*',
+                element: <Navigate to='/browse' replace />,
+            },
+        ],
     },
 ]);
 
